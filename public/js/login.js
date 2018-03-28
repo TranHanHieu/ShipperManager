204f8f963481971d6d2b8146eecc31f17db92e5a
ducthangwru <ducthangwru97@gmail.com>
2018-03-28 19:58:31 +0700

    update login

---
+++ public/js/login.js
@@ -0,0 +1,59 @@
+var isRememberPass = false
+$( document ).ready(function() {
+    var data = JSON.parse(localStorage.getItem("rememberpass"))
+    if(data){
+        $('#username').val(data.username)
+        $('#password').val(data.password)
+        $('#checkRemenberPass').prop('checked',true)
+
+    }
+
+});
+$('#checkRemenberPass').click(function () {
+    isRememberPass = !isRememberPass
+    $(this).prop('checked',isRememberPass)
+})
+$('#btnLogin').click(function (event, err) {
+    // alert('pppppppp'+err);
+    if(!err) {
+        event.preventDefault();
+        let username = $('#username').val();
+        let password = $('#password').val();
+        let tokenfirebase = ""
+        if (!username || !password) {
+            alert('Vui lòng nhập tài khoản hoặc mật khẩu!')
+        } else {
+
+            $.ajax('/api/user/login', {
+                type: "POST",
+                data: {
+                    username, password
+                },
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    if (data.status) {
+                        if(isRememberPass){
+                            localStorage.setItem('rememberpass', JSON.stringify({"username":username,"password":password}));
+                        }
+                        localStorage.setItem('user', JSON.stringify(data.data));
+                        window.location.href = "/"
+                        // $(event.currentTarget).trigger('click', {'handle': true});
+                    } else {
+                        alert(data.msg)
+
+                    }
+                },
+                error: function (err) {
+                    console.log(err)
+                    window.location.href = "/"
+
+                    alert('Lỗi! Không có kết nối, vui lòng thử lại sau.' + err)
+                }
+            });
+        }
+    }else {
+        return
+    }
+
+})
