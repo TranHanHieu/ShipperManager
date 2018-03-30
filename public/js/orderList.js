d487ba7ba92628684c6ba517c761482edcd39135
ducthangwru <ducthangwru97@gmail.com>
2018-03-30 11:55:26 +0700

    update order js, handlebars

--- public/js/orderList.js
+++ public/js/orderList.js
@@ -1,8 +1,40 @@
 document.title = "Danh sách đơn hàng - ShipperManager"
+function deleteOrder(id)
+{
+    console.log(id);
+    if(confirm("Bạn có chắc chắn muốn xóa đơn hàng này?"))
+    {
+        $.ajax('/api/order/deleteOrder?idOrder=' + id, {
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                alert(data.msg);
+                loadDataOrder();
+            },
+            error: function (err) {
+                console.log(err);
+                window.location.href = "/orderList"
+
+                alert('Lỗi! Không có kết nối, vui lòng thử lại sau.' + err)
+            }
+        });
+    }
+}
 
 $( document ).ready(function() {
-    var user = JSON.parse(localStorage.getItem("user"));
+
+    function initMap() {
+        var user = JSON.parse(localStorage.getItem("user"));
+
+        var map = new google.maps.Map(document.getElementById('map2'), {
+            zoom: 13,
+            center: new google.maps.LatLng(user.latitude, user.longitude),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+    }
+    
     if(user){
+
         function loadDataOrder(){
             $.ajax('/api/order/all?idlogin=' + user._id + "&isadmin=" + user.group.isadmin, {
                 type: "GET",
@@ -12,6 +44,8 @@ $( document ).ready(function() {
                     {
                         $("#clEdit").css('display', '');
                         $("#clDelete").css('display', '');
+                        $('#btnAddNewOrder').css('float', 'left');
+                        $('#btnAddNewOrder').css('display', '');
                     }
 
                     $("#bodyOrderList").html("");
@@ -51,11 +85,11 @@ $( document ).ready(function() {
                         {
                             html +=
                             '<td>' +
-                                '<a onclick="editOrder(' + value._id + ')">' +
+                                '<a onclick=editOrder(' + value._id + ')>' +
                                     '<img height="16" width="16" src="images/edit.png">' +
                                 '</a>' +
                             '</td>' +
-                            '<td><a onclick="deleteOrder(' + value._id + ')" data-toggle="modal" data-target="#modalDelete"><img ' +
+                            '<td><a onclick="deleteOrder(\'' + value._id + '\')"><img ' +
                                     'height="16" width="16" src="images/delete.png"></a></td>' +
                             '</tr>'
                         }
@@ -64,23 +98,28 @@ $( document ).ready(function() {
                       });
                 },
                 error: function (err) {
-                    window.location.href = "/employeeList"
+                    window.location.href = "/orderList"
 
                     alert('Lỗi! Không có kết nối, vui lòng thử lại sau.' + err)
                 }
             });
         }
+
+       
+
         
         loadDataOrder();
+        initMap();
         // setInterval(function(){
         //     loadDataOrder();
         // }, 5000);
 
-        function deleteOrder(id)
-        {
-            
-        }
-
+        
     }
+
+    $("#btnAddNewOrder").click(function() {
+        window.location.href = "/addOrder"
+    });
+
 });
 
