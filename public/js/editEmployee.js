1094ac8f9575e045d1d4a37fe959205f8932dda2
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2018-04-02 13:54:50 +0700

    chuyển sang file js

--- public/js/editEmployee.js
+++ public/js/editEmployee.js
@@ -3,6 +3,43 @@ document.title = "Chỉnh sửa thông tin nhân viên"
 $("#imgInp").change(function () {
     readURL(this);
 });
+$("#btnEdit").click(function (event, err) {
+    let group = $('#chucvunv').val()
+    // let username = $('#username').val();
+    let email = $('#emailnv').val();
+    let fullname = $('#fullnamenv').val();
+    let dateofbirth = $('#ngaysinhnv').val();
+    let avatar=''
+    let _id = new URL(window.location.href).searchParams.get('id');
+    console.log(group)
+
+    if (email != "" && fullname != "" && dateofbirth != '') {
+        $.ajax('/api/user/', {
+            type: "PUT",
+            data: {
+                _id,email, fullname, dateofbirth, group,avatar
+            },
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                if (data.status) {
+                    window.location.href = "/employeeList"
+                } else {
+                    alert(data.msg)
+
+                }
+            },
+            error: function (err) {
+                console.log(err)
+
+                alert('Lỗi! Không có kết nối, vui lòng thử lại sau.' + err)
+            }
+        });
+    } else {
+        alert("Vui lòng nhập đầy đủ thông tin nhân viên hoặc mật khẩu xác nhận không đúng!")
+    }
+});
+
 
 function cancelClick() {
     window.location.href = "/employeeList";
